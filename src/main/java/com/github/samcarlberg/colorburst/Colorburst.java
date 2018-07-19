16df545cc0acf66e8344c45d7093244641ff6a62
Sam Carlberg <sam.carlberg@gmail.com>
2018-07-18 22:58:39 -0400

    Add app icons at 16, 32, 64, and 128px. Set minimum window size

--- src/main/java/com/github/samcarlberg/colorburst/Colorburst.java
+++ src/main/java/com/github/samcarlberg/colorburst/Colorburst.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -19,7 +20,16 @@ public final class Colorburst extends Application {
   public void start(Stage stage) throws IOException {
     Pane rootPane = FXMLLoader.load(MainWindowController.class.getResource("MainWindow.fxml"));
     stage.setScene(new Scene(rootPane));
+    stage.setMinWidth(1000);
+    stage.setMinHeight(600);
+    stage.getScene().getStylesheets().add("/com/github/samcarlberg/colorburst/midnight.css");
     stage.setTitle("Colorburst");
+    stage.getIcons().addAll(
+        new Image(Colorburst.class.getResourceAsStream("/com/github/samcarlberg/colorburst/icon-16.png")),
+        new Image(Colorburst.class.getResourceAsStream("/com/github/samcarlberg/colorburst/icon-32.png")),
+        new Image(Colorburst.class.getResourceAsStream("/com/github/samcarlberg/colorburst/icon-64.png")),
+        new Image(Colorburst.class.getResourceAsStream("/com/github/samcarlberg/colorburst/icon-128.png"))
+    );
     stage.setOnCloseRequest(e -> {
       Platform.exit();
     });
